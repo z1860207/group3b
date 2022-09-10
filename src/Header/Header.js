c533becbc0caf248dd7ab114e8092c52291c38c1
JordanRaiaNIU <z1887657@students.niu.edu>
2022-09-10 18:33:39 -0500

    added sign in button

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,12 +7,13 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Header() {
     console.log(defaultLink);
 
     return (
-        <Navbar bg="dark" expand="lg" variant="dark">
+        <Navbar sticky="top" bg="dark" expand="lg" variant="dark">
             <Container>
                 {/* Logo or Title Here */}
                 <Link to={defaultLink} style={{ textDecoration: "none" }}>
@@ -30,23 +31,45 @@ function Header() {
                             <Nav.Link>Home</Nav.Link>
                         </Link>
                         <Link
-                            to={defaultLink + ""}
+                            to={defaultLink + "/"}
                             style={{ textDecoration: "none" }}
                         >
                             <Nav.Link>Link1</Nav.Link>
                         </Link>
                         <Link
-                            to={defaultLink + ""}
+                            to={defaultLink + "/"}
                             style={{ textDecoration: "none" }}
                         >
                             <Nav.Link>Link2</Nav.Link>
                         </Link>
                         <Link
-                            to={defaultLink + ""}
+                            to={defaultLink + "/"}
                             style={{ textDecoration: "none" }}
                         >
                             <Nav.Link>Link3</Nav.Link>
                         </Link>
+                        {/* Dropdown Links */}
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <Link
+                                to={defaultLink + "/"}
+                                style={{ textDecoration: "none" }}
+                            >
+                                <NavDropdown.Item>Drop1</NavDropdown.Item>
+                            </Link>
+                            <Link
+                                to={defaultLink + "/"}
+                                style={{ textDecoration: "none" }}
+                            >
+                                <NavDropdown.Item>Drop2</NavDropdown.Item>
+                            </Link>
+                            <NavDropdown.Divider />
+                            <Link
+                                to={defaultLink + "/"}
+                                style={{ textDecoration: "none" }}
+                            >
+                                <NavDropdown.Item>Drop3</NavDropdown.Item>
+                            </Link>
+                        </NavDropdown>
                     </Nav>
                     <Form className="d-flex">
                         <Form.Control
@@ -57,6 +80,7 @@ function Header() {
                         />
                         <Button variant="outline-success">Search</Button>
                     </Form>
+                    <Button style={{ marginLeft: "8px"}} variant="outline-primary">Sign In</Button>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 // default link to direct to
-export const defaultLink = "/Group3B/";
+export const defaultLink = "/Group3B";
