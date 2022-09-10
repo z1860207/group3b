d805f76b7111787fe3f8f12e27f70f86f86066de
JordanRaiaNIU <z1887657@students.niu.edu>
2022-09-10 15:19:19 -0500

    added redirect from / to /home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./Home/Home";
 import Header from "./Header/Header";
 
@@ -8,7 +8,10 @@ function App() {
         <BrowserRouter>
             <div className="app">
                 <Routes>
-                    <Route path="/" element={[<Header />, <Home />]} />
+                    {/* Route to redirect "/" to "/home" */}
+                    <Route path="/" element={<Navigate to="/home" />} />
+                    {/* Route for Home Page */}
+                    <Route path="/home" element={[<Header />, <Home />]} />
                 </Routes>
             </div>
         </BrowserRouter>
