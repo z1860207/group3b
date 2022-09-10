8e5615ac1084bc8f89645fe7d913497987952595
JordanRaiaNIU <z1887657@students.niu.edu>
2022-09-10 16:51:47 -0500

    added react-bootstrap navbar

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,18 +1,31 @@
 import React from "react";
-import {defaultLink} from "../constants"
+import { Link } from "react-router-dom";
+import { defaultLink } from "../constants";
 // react-bootstrap
-import Container from "react-bootstrap/container"
+import Container from "react-bootstrap/container";
 import Navbar from "react-bootstrap/Navbar";
-
+import Nav from "react-bootstrap/Nav"
 
 function Header() {
     console.log(defaultLink);
 
     return (
-        <Navbar bg="dark" expand="lg">
+        // 
+        <Navbar bg="secondary" expand="lg">
             <Container>
-                <Navbar.Brand href={defaultLink}>Title</Navbar.Brand>
+                {/* The Website Title or Logo will go here */}
+                <Link to={defaultLink}>
+                    <Navbar.Brand style={{ color: "white"}}>Title</Navbar.Brand>
+                </Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        {/* Nav bar links to other parts of website go here */}
+                        <Link to={defaultLink}>
+                            <Nav.Link>Home</Nav.Link>
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
