4673fcf8047bddee269bfeac80ccf13a43bcc577
Edward Ramos <z1860207@turing.cs.niu.edu>
2022-09-16 07:29:45 -0500

    added beginnings of js form submission to JSON export to a php backend host. work in progress

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,13 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
-// react-bootstreap
+// react-bootstrap
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { defaultLink } from "../constants";
 
-function Login() {
-    return (
+function Login() {     
+    function export_JSON () {
+        const form = document.getElementById('login__form');
+        const input_email = form.elements['formBasicEmail']
+        const input_password = form.elements['formBasicPassword']
+        
+        let xhr = new XMLHttpRequest();
+        let url = "https://students.cs.niu.edu/~z1860207/Group3B/submit.php";
+    
+        xhr.open("POST", url, true);
+    
+            // Set the request header i.e. which type of content you are sending
+            xhr.setRequestHeader("Content-Type", "application/json");
+     
+            // Create a state change callback
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    // TODO:ERROR - Print received data from server
+                    //result.innerHTML = this.responseText;
+         
+                }
+        };
+    
+        var user_credentials = JSON.stringify({"email": input_email, "password": input_password});
+        xhr.send(user_credentials);       
+}
+
+    return (  
         <div className="login">
             <Link to={defaultLink} style={{ textDecoration: "none" }}>
                 <h1 className="login__logo">Logo</h1>
@@ -22,7 +48,7 @@ function Login() {
                     {/* Password input */}
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password"/>
                     </Form.Group>
                     {/* Save Login Details Checkbox */}
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -35,7 +61,7 @@ function Login() {
                             </Button>
                         </div>
                         <div className="login__button">
-                            <Button variant="primary" type="submit">
+                            <Button variant="primary" type="submit" onclick={export_JSON}>
                                 Create Account
                             </Button>
                         </div>
@@ -45,5 +71,5 @@ function Login() {
         </div>
     );
 }
-
 export default Login;
+
