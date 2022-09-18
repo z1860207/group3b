ed7bdf8d14d0ee31195d048c3e64a2b53a7cdaca
z1860207 <clipper5813@aol.com>
2022-09-18 05:58:51 -0500

    working form data

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,91 +1,109 @@
-import React from "react"; 
-import axios from 'axios';
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
+import { defaultLink } from "../constants";
 // react-bootstrap
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { defaultLink } from "../constants";
 
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-function Login() {     
+    const signIn = (e) => {
+        e.preventDefault();
 
-    /*function export_JSON (e, p) {
-        //window.event.preventDefault()
-        let input_email = e
-        let input_password = p
-        
+        //do something with email and password
+        console.log(email, password);
+    };
+
+    const register = (e) => {
+        e.preventDefault();
+
+        //do something with email and password
+        console.log(email, password);
+    };
+
+    function export_JSON() {
+        const form = document.getElementById("login__form");
+        const input_email = form.elements["formBasicEmail"];
+        const input_password = form.elements["formBasicPassword"];
+
         let xhr = new XMLHttpRequest();
         let url = "https://students.cs.niu.edu/~z1860207/Group3B/submit.php";
-    
-        xhr.open("POST", url, true);
-    
-            // Set the request header i.e. which type of content you are sending
-            xhr.setRequestHeader("Content-Type", "application/json");
-     
-            // Create a state change callback
-            xhr.onreadystatechange = function () {
-                console.log(xhr.readyState);
-                console.log(xhr.status);
-            };
-    
-        var user_credentials = JSON.stringify({"email": input_email, "password": input_password});
-        xhr.send(user_credentials);       
-    }*/
-    const handleSubmit = (e) => {
-        const axios = require('axios');
 
-        async function doPostRequest() {
-    
-        let payload = { email: 'garythesnail69@yahoo.com', password: 'meow' };
-    
-        let res = await axios.post('https://students.cs.niu.edu/~z1860207/Group3B/submit.php', payload);
-    
-        let data = res.data;
-        console.log(data);
+        xhr.open("POST", url, true);
+
+        // Set the request header i.e. which type of content you are sending
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        // Create a state change callback
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                // TODO:ERROR - Print received data from server
+                //result.innerHTML = this.responseText;
+            }
+        };
+
+        var user_credentials = JSON.stringify({
+            email: input_email,
+            password: input_password,
+        });
+        xhr.send(user_credentials);
     }
-    
-        doPostRequest();
-        //e.preventDefault()
-        /*let user = {
-            input_email: document.getElementById(formBasicEmail).value,
-            input_password: document.getElementById(formBasicPassword).value
-        }*/
-        //export_JSON(document.getElementById('formBasicEmail'), document.getElementById('formBasicPassword'))
-    }
-    return (  
+
+    return (
         <div className="login">
             <Link to={defaultLink} style={{ textDecoration: "none" }}>
                 <h1 className="login__logo">Logo</h1>
             </Link>
             <div className="login__border">
                 <Form className="login__form">
+                    {/* Email input */}
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email Address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control
+                            type="email"
+                            placeholder="Enter email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </Form.Group>
                     {/* Password input */}
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password"/>
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </Form.Group>
                     {/* Save Login Details Checkbox */}
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Remember Me" />
+                        <Form.Check type="checkbox" label="Save Credentials" />
                     </Form.Group>
                     <div className="login__signInButtons">
                         <div className="login__button">
-                            <Button variant="primary" type="submit" controlID="login_btn">
+                            <Button
+                                variant="primary"
+                                type="submit"
+                                onClick={signIn}
+                            >
                                 Sign In
                             </Button>
                         </div>
                         <div className="login__button">
-                            <Button variant="primary" onClick={handleSubmit} type="submit" controlId="create_btn">
+                            <Button
+                                variant="primary"
+                                type="submit"
+                                onClick={register}
+                            >
                                 Create Account
                             </Button>
                         </div>
                     </div>
-                </Form>   
+                </Form>
             </div>
         </div>
     );
---
+++ src/Login/LoginBackup.js
@@ -0,0 +1,73 @@
+/*import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+// react-bootstrap
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { defaultLink } from "../constants";
+
+function Login() {     
+    return (  
+        <div className="login">
+            <Link to={defaultLink} style={{ textDecoration: "none" }}>
+                <h1 className="login__logo">Logo</h1>
+            </Link>
+            <div className="login__border">
+                <Form className="login__form">
+                    {/* Email input */}
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    {/* Password input */}
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password"/>
+                    </Form.Group>
+                    {/* Save Login Details Checkbox */}
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Save Credentials" />
+                    </Form.Group>
+                    <div className="login__signInButtons">
+                        <div className="login__button">
+                            <Button variant="primary" type="submit">
+                                Sign In
+                            </Button>
+                        </div>
+                        <div className="login__button">
+                            <Button variant="primary" type="submit">
+                                Create Account
+                            </Button>
+                        </div>
+                    </div>
+                </Form>
+            </div>
+        </div>
+    );
+}
+export default Login;
+    function export_JSON () {
+        const form = document.getElementById('login__form');
+        const input_email = form.elements['formBasicEmail']
+        const input_password = form.elements['formBasicPassword']
+        
+        let xhr = new XMLHttpRequest();
+        let url = "https://students.cs.niu.edu/~z1860207/Group3B/submit.php";
+    
+        xhr.open("POST", url, true);
+    
+            // Set the request header i.e. which type of content you are sending
+            xhr.setRequestHeader("Content-Type", "application/json");
+     
+            // Create a state change callback
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    // TODO:ERROR - Print received data from server
+                    //result.innerHTML = this.responseText;
+         
+                }
+        };
+    
+        var user_credentials = JSON.stringify({"email": input_email, "password": input_password});
+        xhr.send(user_credentials);       
+}*/
